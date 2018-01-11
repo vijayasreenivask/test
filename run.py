664f844790aa687e42533b3ab620997e0a60584e
vijayasreenivask <vijayasreenivask@hcl.com>
2018-01-11 15:45:28 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,4 @@
-print "TEST API"
-print "TEST API"
-print "TEST API"
-print "TEST API"
-print "TEST API"
-print "TEST API"
+from test import app, db
+if __name__ == '__main__':
+    db.create_all()
+    app.run(host='0.0.0.0', port=5000, debug=True)
